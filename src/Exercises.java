e8232356167d46f5463f86a15d89cce40532c311
yokarak <ande67f3@edu.easj.dk>
2019-09-08 20:30:22 +0200

    exercise 1.1

---
+++ src/Exercises.java
@@ -0,0 +1,18 @@
+public class Exercises {
+    public static void main(String[] args) {
+
+        // 1.1 (display three messages) write a program that displays Welcome to java, Learning java now and Programming is fun
+
+        System.out.println("Welcome to Java");
+        System.out.println(" Learning Java now");
+        System.out.println("Programming is Fun");
+
+
+
+
+
+
+    }
+
+
+}
