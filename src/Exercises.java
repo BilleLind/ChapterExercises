4c2fe0ff9f99950da036d3a474055eb6eb70edf4
yokarak <ande67f3@edu.easj.dk>
2019-09-08 20:37:33 +0200

    exercise 1.3

--- src/Exercises.java
+++ src/Exercises.java
@@ -7,11 +7,22 @@ public class Exercises {
         System.out.println(" Learning Java now");
         System.out.println("Programming is Fun");
 
-        //1.2 write a program that displays "I love Java" five times
+        // 1.2 write a program that displays "I love Java" five times
         int i;
         for (i = 0; i < 5; i++)
             System.out.println("I love java");
 
+        /* 1.3 Write a program that displats the following patterne:
+            j
+        j   aaa    v     vaaa
+        j   j  aa    v v    a a
+        j    aaaa     v     aaaa
+         */
+
+        System.out.println("" +
+                "        j   aaa    v     vaaa\n" +
+                "        j   j  aa    v v    a a\n" +
+                "        j    aaaa     v     aaaa");
 
 
 
